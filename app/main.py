3a4a8760912c046a3f3dcfd825fd3489f61007b1
Oleh Kolodka <oleh1928@gmail.com>
2024-04-17 12:06:11 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import json
+
 from app.shop import Shop
 from app.customer import Customer
 
@@ -7,8 +8,8 @@ def shop_trip() -> None:
     with open("app/config.json", "r") as file_config:
         config = json.load(file_config)
 
-        fuel_price = config["FUEL_PRICE"]
-        shops = [Shop(**shop) for shop in config["shops"]]
+    fuel_price = config["FUEL_PRICE"]
+    shops = [Shop(**shop) for shop in config["shops"]]
 
-        for customer in config["customers"]:
-            Customer(**customer).go_shopping(fuel_price, shops)
+    for customer in config["customers"]:
+        Customer(**customer).go_shopping(fuel_price, shops)
